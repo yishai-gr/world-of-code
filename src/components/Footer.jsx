f11cccf8015691a3c0d478b25f6a7cbcf9d9daae
yishai-gr <yishai5765@gmail.com>
2024-06-16 11:00:53 +0300

    try to fix issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer>
       <div className="brand">
         <img src="/logo.png" alt="logo" width={100} />
-        <p>World of CODE</p>
+        <p>World of CODE V0.0.1</p>
       </div>
       <Nav />
       <div></div>
